c683f50c54be032ec79fb803dc1584b250d0c289
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2023-08-14 09:26:02 +0200

    + / - satsar i dropdown

--- client/src/kalkulator/FormatertStyrke.tsx
+++ client/src/kalkulator/FormatertStyrke.tsx
@@ -1,5 +1,3 @@
-import { TFunction, useTranslation } from 'react-i18next'
-import { MAX_SFÆRE, MAX_STYRKE, MAX_SYLINDER, MIN_STYRKE } from './config'
 
 interface FormatertStyrkeProps {
   verdi?: number | string
@@ -8,28 +6,27 @@ interface FormatertStyrkeProps {
 
 export function FormatertStyrke(props: FormatertStyrkeProps) {
   const { verdi, type } = props
-  const { t } = useTranslation()
   if (verdi == null || verdi === '') {
     return null
   }
   switch (type) {
     case 'sfære':
-      return <>{formater(t, +Number(verdi), +1, +MAX_SFÆRE)}</>
+      if(verdi < 0 ){
+        return <>-{formater(-Number(verdi))}</>
+      } else if(verdi > 0){
+        return <>+{formater(+Number(verdi))}</>
+      } else {
+        return  <>{formater(+Number(verdi))}</>
+      }
+
     case 'sylinder':
-      return <>{formater(t, -Number(verdi), -1, -MAX_SYLINDER)}</>
+      return <>{formater(-Number(verdi))}</>
     default:
       return null
   }
 }
 
-function formater(t: TFunction, verdi: number, min: number, max: number) {
-  const styrke = Math.abs(verdi)
-  if (styrke === MIN_STYRKE) {
-    return t('kalkulator.styrke_under', { min: formatter.format(min) })
-  }
-  if (styrke === MAX_STYRKE) {
-    return t('kalkulator.styrke_over', { max: formatter.format(max) })
-  }
+function formater(verdi: number) {
   return formatter.format(verdi)
 }
 
