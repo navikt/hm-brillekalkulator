f4973eb6195266d6289930774781d6d0aa439ed3
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2023-09-27 13:43:31 +0200

    Infoboks om kalkulator

--- client/src/kalkulator/Kalkulator.tsx
+++ client/src/kalkulator/Kalkulator.tsx
@@ -1,24 +1,24 @@
-import {Heading} from '@navikt/ds-react'
-import {useTranslation} from 'react-i18next'
-import {Banner} from '../components/Banner'
-import {HotjarTrigger} from '../components/hotjar-trigger'
-import {ScrollToTop} from '../components/ScrollToTop'
-import {KalkulatorForm} from './KalkulatorForm'
+import { Alert, BodyShort, Heading } from '@navikt/ds-react'
+import { useTranslation } from 'react-i18next'
+import { Banner } from '../components/Banner'
+import { ScrollToTop } from '../components/ScrollToTop'
+import { KalkulatorForm } from './KalkulatorForm'
+import { Avstand } from '../components/Avstand'
 
 export function Kalkulator() {
-    const {t} = useTranslation()
-    return (
-        <>
-            <header>
-                <Banner>
-                    <Heading level="1" size="large">
-                        {t('kalkulator.overskrift')}
-                    </Heading>
-                </Banner>
-            </header>
-            <main>
-                <ScrollToTop/>
-                {/* <Avstand marginBottom={5} marginTop={5}>
+  const { t } = useTranslation()
+  return (
+    <>
+      <header>
+        <Banner>
+          <Heading level="1" size="large">
+            {t('kalkulator.overskrift')}
+          </Heading>
+        </Banner>
+      </header>
+      <main>
+        <ScrollToTop />
+        {/* <Avstand marginBottom={5} marginTop={5}>
                     <Endringsvarsel
                         tittel={t('info.satsendring.tittel')}
                         tekst={t('info.satsendring.tekst')}
@@ -26,8 +26,13 @@ export function Kalkulator() {
                         lenke="https://www.nav.no/briller-til-barn"
                     />
                 </Avstand> */}
-                <KalkulatorForm/>
-            </main>
-        </>
-    )
+        <Avstand marginBottom={5} marginTop={5}>
+          <Alert variant="info">
+            <BodyShort>{t('kalkulator.informasjon_om_kalkulator')}</BodyShort>
+          </Alert>
+        </Avstand>
+        <KalkulatorForm />
+      </main>
+    </>
+  )
 }
