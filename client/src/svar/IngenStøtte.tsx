a458f1b10868d7984e70756f265bd84ad2c26712
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2023-10-02 12:15:05 +0200

    Statisk tekst til oversettelse

--- client/src/svar/IngenStøtte.tsx
+++ client/src/svar/IngenStøtte.tsx
@@ -1,14 +1,8 @@
-import { Heading } from "@navikt/ds-react";
-import { useTranslation } from "react-i18next";
-import React from "react";
-import styled from "styled-components";
-import { Vilkårsvurdering } from "../kalkulator/useVilkårsvurdering";
-import {
-  CheckmarkCircleFillIcon,
-  InformationSquareFillIcon,
-  XMarkOctagonFillIcon,
-  XMarkOctagonIcon
-} from "@navikt/aksel-icons";
+import { Heading } from '@navikt/ds-react'
+import { useTranslation } from 'react-i18next'
+import React from 'react'
+import styled from 'styled-components'
+import { InformationSquareFillIcon, XMarkOctagonFillIcon } from '@navikt/aksel-icons'
 
 export function IngenStøtte() {
   const { t } = useTranslation()
@@ -16,16 +10,13 @@ export function IngenStøtte() {
   return (
     <>
       <FailureTop>
-        <XMarkOctagonFillIcon title="a11y-title" fontSize="1.5rem" color="#F68282"/>
+        <XMarkOctagonFillIcon title="a11y-title" fontSize="1.5rem" color="#F68282" />
         <Heading level="2" size="medium">
           {t('kalkulator.vilkårsvurdering_ikke_ok')}
         </Heading>
         <div style={{ gridColumnStart: 2, display: 'flex', justifyContent: 'flex-start', padding: '1rem 0' }}>
           <InformationSquareFillIcon title="a11y-title" fontSize="1.5rem" color="#236B7D" />
-          <div style={{ width: '80%', paddingLeft: '0.5rem' }}>
-            Dette er kun et veiledende svar. Endelig svar får du etter et besøk hos optiker eller etter at NAV
-            har behandlet innsendt søknad.
-          </div>
+          <div style={{ width: '80%', paddingLeft: '0.5rem' }}>{t('kalkulator.informasjon_om_veiledende_svar')}</div>
         </div>
       </FailureTop>
     </>
