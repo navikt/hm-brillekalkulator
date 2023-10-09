dc9ced14ae1b2271b61a2d18fbeb72b1cc7601fe
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2023-10-09 10:09:26 +0200

    Tekstendringer

--- client/src/kalkulator/Kalkulator.tsx
+++ client/src/kalkulator/Kalkulator.tsx
@@ -1,9 +1,10 @@
-import { Alert, BodyShort, Heading } from '@navikt/ds-react'
+import { Alert, BodyShort, GuidePanel, Heading } from "@navikt/ds-react";
 import { useTranslation } from 'react-i18next'
 import { Banner } from '../components/Banner'
 import { ScrollToTop } from '../components/ScrollToTop'
 import { KalkulatorForm } from './KalkulatorForm'
 import { Avstand } from '../components/Avstand'
+import React from "react";
 
 export function Kalkulator() {
   const { t } = useTranslation()
@@ -26,10 +27,10 @@ export function Kalkulator() {
                         lenke="https://www.nav.no/briller-til-barn"
                     />
                 </Avstand> */}
-        <Avstand marginBottom={5} marginTop={5}>
-          <Alert variant="info">
-            <BodyShort>{t('kalkulator.informasjon_om_kalkulator')}</BodyShort>
-          </Alert>
+        <Avstand marginBottom={10} marginTop={5}>
+          <GuidePanel>
+            {t('kalkulator.informasjon_om_kalkulator')}
+          </GuidePanel>
         </Avstand>
         <KalkulatorForm />
       </main>
